b71a8d5883b75c634eabee1f29bf109059c1f4a0
Alexander Lichter <github@lichter.io>
2023-02-15 19:56:09 +0100

    chore: add route rules

--- packages/api/nitro.config.ts
+++ packages/api/nitro.config.ts
@@ -1,6 +1,12 @@
 import { defineNitroConfig } from "nitropack";
 
 export default defineNitroConfig({
+  routeRules: {
+    '/**': {
+      cors: true,
+      headers: { 'access-control-allow-methods': 'GET' }
+    }
+  },
   runtimeConfig: {
     mongodbConnection: ''
   }
