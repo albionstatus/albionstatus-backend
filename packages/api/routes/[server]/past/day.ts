c69dbae40551f151925bb32a400922c317a2f7f9
Alexander Lichter <github@lichter.io>
2023-02-20 12:53:08 +0100

    feat: add day endpoint

--- packages/api/routes/[server]/past/day.ts
+++ packages/api/routes/[server]/past/day.ts
@@ -1,6 +1,6 @@
-import { subWeeks } from 'date-fns'
 import { createDbClient } from '../../../../shared/db.js'
 import type { ServerName } from '../../../../shared/types.js'
+import { subDays } from 'date-fns'
 
 const { realmAppId: rawAppId } = useRuntimeConfig()
 // @ts-expect-error NITRO_REALM_APP_ID is injected by Cloudflare Workers
@@ -9,7 +9,7 @@ const appId = rawAppId || NITRO_REALM_APP_ID as string
 export default defineCachedEventHandler(async (event) => {
   const { server } = event.context.params
 
-  const timestamp = subWeeks(new Date(), 1)
+  const timestamp = subDays(new Date(), 1)
 
   const { getPastStatuses } = await createDbClient({
     appId,
